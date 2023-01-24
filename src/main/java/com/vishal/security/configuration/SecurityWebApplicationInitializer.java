75a48f718edf20d8467a46658888669e011e5475
vishal1273 <vishalthakur107@gmail.com>
2023-01-25 02:53:19 +0530

    Add Custom Login Form

---
+++ src/main/java/com/vishal/security/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.vishal.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
